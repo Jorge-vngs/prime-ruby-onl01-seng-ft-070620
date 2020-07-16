fd13d15847d505e1b334c750000f410f4ebef53b
Jorge Venegas <brangelina01@gmail.com>
2020-07-16 03:19:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
-
-def read_array(array)
-     primes = []
-  array.each do |num|
-       primes << num if is_prime?(num)
+def prime?(x)
+  start = 2
+  if x > 1
+    range = (start..x-1).to_a
+    range.none? do |num_to_test| 
+      x % num_to_test == 0
+    end
+  else
+    false
   end
- return primes
 end
